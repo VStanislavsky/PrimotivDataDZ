bdfc8e89aee52948e978bbec93de38878f45e052
VStanislavsky <stanislavvlasov@mail.ru>
2021-12-29 13:59:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 2_050;
+        int oneMile = 20;
+        int totalMiles = ticketPrice / oneMile;
+        System.out.println(totalMiles);
+    }
+}
